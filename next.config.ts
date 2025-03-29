ecf0468bdbaaceae3ee11cfe2c47a133b322d2bd
808vita <its808vita@gmail.com>
2025-03-29 23:09:18 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["beeai-framework", "zod"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
